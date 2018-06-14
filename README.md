55ea04a83160d1d68607125dc987dc3c03a2fe8b
anotherdev <github.com@anotherdev.com>
2018-06-14 12:28:24 +0700

    remove public api key from default

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # SimpleChat
 
 https://docs.ekomedia.technology/
+
+You can try out using this API Key: "b3bee90c39d9a5644831d84e5a0d1688d100ddebef3c6e78".
--- app/src/main/java/com/ekoapp/simplechat/SimpleConfig.java
+++ app/src/main/java/com/ekoapp/simplechat/SimpleConfig.java
@@ -2,7 +2,10 @@ package com.ekoapp.simplechat;
 
 public interface SimpleConfig {
 
-    String DEFAULT_API_KEY = "b3bee90c39d9a5644831d84e5a0d1688d100ddebef3c6e78";
+    /**
+     * You can try out using this API Key: "b3bee90c39d9a5644831d84e5a0d1688d100ddebef3c6e78".
+     */
+    String DEFAULT_API_KEY = "";
     String DEFAULT_USER_ID = "victimAndroid";
     String DEFAULT_ADMIN_USER_ID = "ADMIN";
 }
