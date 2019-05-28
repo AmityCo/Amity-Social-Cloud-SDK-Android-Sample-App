d3b47026e8f9e446c52d859516667773cda37e58
Pan <theptai@ekoapp.com>
2019-05-28 16:46:24 +0700

    fcm impl

---
+++ app/src/main/java/com/ekoapp/simplechat/SimpleFirebaseMessagingService.java
@@ -0,0 +1,11 @@
+package com.ekoapp.simplechat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class SimpleFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(String token) {
+
+    }
+}
