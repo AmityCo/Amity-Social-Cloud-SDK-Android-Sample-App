9a232cdbaa1c2648a74492e57c2cdaa22ba1d858
Best <nakarin@ekoapp.com>
2019-10-11 21:23:06 +0700

    add download library

--- app/src/main/java/com/ekoapp/simplechat/IntentRequestCode.java
+++
@@ -1,9 +0,0 @@
-package com.ekoapp.simplechat;
-
-public class IntentRequestCode {
-
-    public static final int REQUEST_TAKE_PHOTO = 10001;
-    public static final int REQUEST_SELECT_PHOTO = 20002;
-    public static final int REQUEST_SELECT_FILE = 30003;
-
-}
