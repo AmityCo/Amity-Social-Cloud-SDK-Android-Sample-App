a41041210c20cdbd2ab9d4ff22137af677eba92a
Chattip Soontaku <chattip@ekoapp.com>
2020-04-26 00:56:27 +0700

    start chat in first page

--- app/src/main/java/com/ekoapp/view/MainNavigationActivity.kt
+++ app/src/main/java/com/ekoapp/view/MainNavigationActivity.kt
@@ -6,10 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import com.ekoapp.di.DaggerActivityComponent
 import com.ekoapp.sample.R
 import com.ekoapp.sample.ui.extensions.coreComponent
-import com.ekoapp.utils.split.SplitInstall
 import com.google.android.play.core.splitinstall.SplitInstallManager
 import com.google.android.play.core.splitinstall.SplitInstallRequest
-import kotlinx.android.synthetic.main.activity_main_navigation.*
 import javax.inject.Inject
 
 
@@ -26,26 +24,11 @@ class MainNavigationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_navigation)
 
-        btn_open.setOnClickListener {
-            SplitInstall(this, installManager, installRequest).installModule {
-                /*when (it) {
-                    is InstallModuleSealed.Installed -> {
-                        if (it.data.module == CHAT_DYNAMIC_FEATURE) {
-                            val intent = Intent().setClassName(
-                                    this,
-                                    "com.ekoapp.sample.chatfeature.channellist.ChannelListActivity"
-                            )
-                            startActivity(intent)
-                        }
-                    }
-                }*/
-                val intent = Intent().setClassName(
-                        this,
-                        "com.ekoapp.sample.chatfeature.channellist.ChannelListActivity"
-                )
-                startActivity(intent)
-            }
-        }
+        val intent = Intent().setClassName(
+                this,
+                "com.ekoapp.sample.chatfeature.channellist.ChannelListActivity"
+        )
+        startActivity(intent)
 
     }
 
