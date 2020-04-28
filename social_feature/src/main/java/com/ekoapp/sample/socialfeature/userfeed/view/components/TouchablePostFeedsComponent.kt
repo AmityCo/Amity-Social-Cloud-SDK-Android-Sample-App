f7686169e8f438d9259919d802b81eb88f93cc7a
Chattip Soontaku <chattip@ekoapp.com>
2020-04-29 00:55:43 +0700

    Created touchable target post

---
+++ social_feature/src/main/java/com/ekoapp/sample/socialfeature/userfeed/view/components/TouchablePostFeedsComponent.kt
@@ -0,0 +1,19 @@
+package com.ekoapp.sample.socialfeature.userfeed.view.components
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.ekoapp.sample.socialfeature.R
+
+
+class TouchablePostFeedsComponent : ConstraintLayout {
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.component_touchable_post_feeds, this, true)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle)
+
+    constructor(context: Context, attrs: AttributeSet) : this(context, attrs, 0)
+}
