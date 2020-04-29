f805b238363bd1ca9e1499d1da7dd798a7642769
Chattip Soontaku <chattip@ekoapp.com>
2020-04-29 08:44:52 +0700

    Improved page not available

--- app/src/main/java/com/ekoapp/sample/main/navigation/fragments/NotAvailable.kt
+++ app/src/main/java/com/ekoapp/sample/main/navigation/fragments/NotAvailable.kt
@@ -6,16 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.ekoapp.sample.R
-import kotlinx.android.synthetic.main.fragment_coming_soon.*
 
-class Users : Fragment() {
+class NotAvailable : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_coming_soon, container, false)
+        return inflater.inflate(R.layout.fragment_not_available, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        tv_coming_soon.text  = "Users"
     }
 }
