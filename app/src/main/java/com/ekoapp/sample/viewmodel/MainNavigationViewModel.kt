6a67f9f3162758ace0a0cd3503c397d087474c45
Chattip Soontaku <chattip@ekoapp.com>
2020-05-02 19:38:15 +0700

    Inject splitInstall

--- app/src/main/java/com/ekoapp/sample/viewmodel/MainNavigationViewModel.kt
+++ app/src/main/java/com/ekoapp/sample/viewmodel/MainNavigationViewModel.kt
@@ -1,24 +1,18 @@
 package com.ekoapp.sample.viewmodel
 
-import android.content.Context
 import com.ekoapp.sample.core.base.viewmodel.DisposableViewModel
 import com.ekoapp.sample.core.utils.getCurrentClassAndMethodNames
 import com.ekoapp.sample.core.utils.splitinstall.CHAT_DYNAMIC_FEATURE
 import com.ekoapp.sample.core.utils.splitinstall.InstallModuleSealed
 import com.ekoapp.sample.core.utils.splitinstall.SOCIAL_DYNAMIC_FEATURE
 import com.ekoapp.sample.core.utils.splitinstall.SplitInstall
-import com.google.android.play.core.splitinstall.SplitInstallManager
 import com.google.android.play.core.splitinstall.SplitInstallRequest
 import timber.log.Timber
 import javax.inject.Inject
 
-class MainNavigationViewModel @Inject constructor(
-        private val context: Context,
-        private val installManager: SplitInstallManager
-) : DisposableViewModel() {
-
+class MainNavigationViewModel @Inject constructor(private val splitInstall: SplitInstall) : DisposableViewModel() {
     fun installModule(installRequest: SplitInstallRequest) {
-        SplitInstall(context, installManager, installRequest).installModule {
+        splitInstall.installModule(installRequest) {
             when (it) {
                 is InstallModuleSealed.Installed -> {
                     when (it.data.module) {
