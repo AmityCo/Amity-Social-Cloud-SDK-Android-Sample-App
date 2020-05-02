aeca0474377eb08563d012ce5a734ff13c86e562
Chattip Soontaku <chattip@ekoapp.com>
2020-05-02 19:04:24 +0700

    Move install module to view model

--- app/src/main/java/com/ekoapp/sample/MainNavigationActivity.kt
+++ app/src/main/java/com/ekoapp/sample/MainNavigationActivity.kt
@@ -6,24 +6,14 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.ekoapp.sample.core.base.viewmodel.SingleViewModelActivity
 import com.ekoapp.sample.core.ui.extensions.coreComponent
-import com.ekoapp.sample.core.utils.getCurrentClassAndMethodNames
-import com.ekoapp.sample.core.utils.splitinstall.CHAT_DYNAMIC_FEATURE
-import com.ekoapp.sample.core.utils.splitinstall.InstallModuleSealed
-import com.ekoapp.sample.core.utils.splitinstall.SOCIAL_DYNAMIC_FEATURE
-import com.ekoapp.sample.core.utils.splitinstall.SplitInstall
 import com.ekoapp.sample.main.di.DaggerMainNavigationComponent
 import com.ekoapp.sample.viewmodel.MainNavigationViewModel
-import com.google.android.play.core.splitinstall.SplitInstallManager
 import com.google.android.play.core.splitinstall.SplitInstallRequest
 import kotlinx.android.synthetic.main.activity_bottom_navigation.*
-import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Named
 
 class MainNavigationActivity : SingleViewModelActivity<MainNavigationViewModel>() {
-
-    @Inject
-    lateinit var splitInstallManager: SplitInstallManager
 
     @Inject
     @Named("social")
@@ -32,7 +22,6 @@ class MainNavigationActivity : SingleViewModelActivity<MainNavigationViewModel>(
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setupAppBar()
-        installModule(socialRequest)
         setUpNavigation()
     }
 
@@ -57,25 +46,8 @@ class MainNavigationActivity : SingleViewModelActivity<MainNavigationViewModel>(
         navHostFragment.findNavController().let { NavigationUI.setupWithNavController(bottom_navigation, it) }
     }
 
-    private fun installModule(installRequest: SplitInstallRequest) {
-        SplitInstall(this, splitInstallManager, installRequest).installModule {
-            when (it) {
-                is InstallModuleSealed.Installed -> {
-                    when (it.data.module) {
-                        CHAT_DYNAMIC_FEATURE -> {
-
-                        }
-                        SOCIAL_DYNAMIC_FEATURE -> {
-                            Timber.d("%s%s", getCurrentClassAndMethodNames(), SOCIAL_DYNAMIC_FEATURE)
-                        }
-                    }
-                }
-            }
-        }
-    }
-
     override fun bindViewModel(viewModel: MainNavigationViewModel) {
-
+        viewModel.installModule(socialRequest)
     }
 
     override fun getViewModelClass(): Class<MainNavigationViewModel> {
--- app/src/main/java/com/ekoapp/sample/viewmodel/MainNavigationViewModel.kt
+++ app/src/main/java/com/ekoapp/sample/viewmodel/MainNavigationViewModel.kt
@@ -2,6 +2,36 @@ package com.ekoapp.sample.viewmodel
 
 import android.content.Context
 import com.ekoapp.sample.core.base.viewmodel.DisposableViewModel
+import com.ekoapp.sample.core.utils.getCurrentClassAndMethodNames
+import com.ekoapp.sample.core.utils.splitinstall.CHAT_DYNAMIC_FEATURE
+import com.ekoapp.sample.core.utils.splitinstall.InstallModuleSealed
+import com.ekoapp.sample.core.utils.splitinstall.SOCIAL_DYNAMIC_FEATURE
+import com.ekoapp.sample.core.utils.splitinstall.SplitInstall
+import com.google.android.play.core.splitinstall.SplitInstallManager
+import com.google.android.play.core.splitinstall.SplitInstallRequest
+import timber.log.Timber
 import javax.inject.Inject
 
-class MainNavigationViewModel @Inject constructor(private val context: Context) : DisposableViewModel()
+class MainNavigationViewModel @Inject constructor(
+        private val context: Context,
+        private val installManager: SplitInstallManager
+) : DisposableViewModel() {
+
+    fun installModule(installRequest: SplitInstallRequest) {
+        SplitInstall(context, installManager, installRequest).installModule {
+            when (it) {
+                is InstallModuleSealed.Installed -> {
+                    when (it.data.module) {
+                        CHAT_DYNAMIC_FEATURE -> {
+                            Timber.d("%s%s", getCurrentClassAndMethodNames(), CHAT_DYNAMIC_FEATURE)
+                        }
+                        SOCIAL_DYNAMIC_FEATURE -> {
+                            Timber.d("%s%s", getCurrentClassAndMethodNames(), SOCIAL_DYNAMIC_FEATURE)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
