53d64551e27b0f79abc2c6ec8e6707e8328bf6ad
Chattip Soontaku <chattip@ekoapp.com>
2020-05-10 10:10:11 +0700

    Improved bottom sheet make it dark theme

--- social_feature/src/main/java/com/ekoapp/sample/socialfeature/userfeed/view/dialogs/MoreHorizBottomSheetFragment.kt
+++ social_feature/src/main/java/com/ekoapp/sample/socialfeature/userfeed/view/dialogs/MoreHorizBottomSheetFragment.kt
@@ -1,10 +1,14 @@
 package com.ekoapp.sample.socialfeature.userfeed.view.dialogs
 
+import android.app.Dialog
+import android.content.DialogInterface
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ekoapp.sample.socialfeature.R
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.android.synthetic.main.bottom_sheet_user_feeds_more_horiz.*
 
@@ -18,6 +22,20 @@ class MoreHorizBottomSheetFragment : BottomSheetDialogFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         fragmentView = inflater.inflate(R.layout.bottom_sheet_user_feeds_more_horiz, container, false)
         return fragmentView
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
+        dialog.setOnShowListener { setupBottomSheet(it) }
+        return dialog
+    }
+
+    private fun setupBottomSheet(dialogInterface: DialogInterface) {
+        val bottomSheetDialog = dialogInterface as BottomSheetDialog
+        val bottomSheet = bottomSheetDialog.findViewById<View>(
+                com.google.android.material.R.id.design_bottom_sheet)
+                ?: return
+        bottomSheet.setBackgroundColor(Color.TRANSPARENT)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
