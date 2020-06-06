77448f473328abec49e5f8dd2766d8662fdcbeca
Chattip Soontaku <chattip@ekoapp.com>
2020-06-06 10:19:32 +0700

    Refactor

--- chat_feature/src/main/java/com/ekoapp/sample/chatfeature/settings/ChannelSettingsViewModel.kt
+++ chat_feature/src/main/java/com/ekoapp/sample/chatfeature/settings/ChannelSettingsViewModel.kt
@@ -1,16 +1,11 @@
 package com.ekoapp.sample.chatfeature.settings
 
 import android.content.Context
-import androidx.lifecycle.MutableLiveData
 import com.ekoapp.sample.chatfeature.R
 import com.ekoapp.sample.core.base.viewmodel.DisposableViewModel
 import javax.inject.Inject
 
 class ChannelSettingsViewModel @Inject constructor(private val context: Context) : DisposableViewModel() {
-
-    var selectedMembership: MutableLiveData<String> = MutableLiveData<String>().apply {
-        this.value = ""
-    }
 
     fun getChannelTypes(): ArrayList<String> {
         val items = ArrayList<String>()
@@ -20,4 +15,6 @@ class ChannelSettingsViewModel @Inject constructor(private val context: Context)
         items.add(context.getString(R.string.temporarily_conversation))
         return items
     }
+
+    fun getMembership(): Array<String> = context.resources.getStringArray(R.array.Membership)
 }
--- chat_feature/src/main/java/com/ekoapp/sample/chatfeature/settings/list/viewholder/MembershipViewHolder.kt
+++ chat_feature/src/main/java/com/ekoapp/sample/chatfeature/settings/list/viewholder/MembershipViewHolder.kt
@@ -4,36 +4,23 @@ import android.content.Context
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import com.ekoapp.ekosdk.EkoChannelFilter
 import com.ekoapp.sample.chatfeature.R
 import com.ekoapp.sample.chatfeature.settings.ChannelSettingsViewModel
 import com.ekoapp.sample.core.base.list.BaseViewHolder
-import com.ekoapp.sample.core.preferences.SimplePreferences
 import kotlinx.android.synthetic.main.item_settings_membership.view.*
 
 class MembershipViewHolder(itemView: View) : BaseViewHolder<ChannelSettingsViewModel>(itemView) {
 
     override fun bind(item: ChannelSettingsViewModel) {
         val context = itemView.context
-        context.setUpSpinner(item)
+        context.renderSpinner(item)
     }
 
-    private fun Context.setUpSpinner(viewModel: ChannelSettingsViewModel) {
-        val modes = mutableListOf(
-                EkoChannelFilter.ALL.apiKey.capitalize(),
-                EkoChannelFilter.MEMBER.apiKey.capitalize(),
-                EkoChannelFilter.NOT_MEMBER.apiKey.capitalize())
-
-        itemView.spinner_membership.adapter = ArrayAdapter(this,
-                R.layout.spinner_item,
-                modes)
-
-        val selectedOption = SimplePreferences.getChannelMembershipOption().get()
-        itemView.spinner_membership.setSelection(modes.indexOf(selectedOption))
-
+    private fun Context.renderSpinner(viewModel: ChannelSettingsViewModel) {
+        itemView.spinner_membership.adapter = ArrayAdapter(this, R.layout.spinner_item, viewModel.getMembership())
         itemView.spinner_membership.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
-                viewModel.selectedMembership.value = modes[position]
+
             }
 
             override fun onNothingSelected(parent: AdapterView<*>) = Unit
