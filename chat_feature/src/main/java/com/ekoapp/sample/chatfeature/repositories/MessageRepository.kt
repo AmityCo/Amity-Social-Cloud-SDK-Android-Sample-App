b81ea0bb688e275b683522255a018baa77492b58
Chattip Soontaku <chattip@ekoapp.com>
2020-06-08 20:31:06 +0700

    function customMessage

--- chat_feature/src/main/java/com/ekoapp/sample/chatfeature/repositories/MessageRepository.kt
+++ chat_feature/src/main/java/com/ekoapp/sample/chatfeature/repositories/MessageRepository.kt
@@ -5,6 +5,7 @@ import androidx.paging.PagedList
 import com.ekoapp.ekosdk.EkoClient
 import com.ekoapp.ekosdk.EkoMessage
 import com.ekoapp.ekosdk.EkoTags
+import com.google.gson.JsonObject
 import io.reactivex.Completable
 import javax.inject.Inject
 
@@ -33,6 +34,16 @@ class MessageRepository @Inject constructor() {
     fun createMessage(channelId: String, text: String): Completable {
         return EkoClient.newMessageRepository().createMessage(channelId)
                 .text(text)
+                .build()
+                .send()
+    }
+
+    fun customMessage(channelId: String, parentId: String? = null, data: JsonObject): Completable {
+        val builder = EkoClient.newMessageRepository().createMessage(channelId).custom(data)
+        return if (parentId.isNullOrEmpty())
+            builder.build()
+                    .send()
+        else builder.parentId(parentId)
                 .build()
                 .send()
     }
