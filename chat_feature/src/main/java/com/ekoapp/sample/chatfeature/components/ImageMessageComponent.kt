3b0be0f2d4cc7d980e688ceac9f5b3e2dee1e18e
Chattip Soontaku <chattip@ekoapp.com>
2020-06-09 11:42:41 +0700

    Create component image message and text message

---
+++ chat_feature/src/main/java/com/ekoapp/sample/chatfeature/components/ImageMessageComponent.kt
@@ -0,0 +1,18 @@
+package com.ekoapp.sample.chatfeature.components
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.ekoapp.sample.chatfeature.R
+
+class ImageMessageComponent : ConstraintLayout {
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.component_image_message, this, true)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle)
+
+    constructor(context: Context, attrs: AttributeSet) : this(context, attrs, 0)
+}
