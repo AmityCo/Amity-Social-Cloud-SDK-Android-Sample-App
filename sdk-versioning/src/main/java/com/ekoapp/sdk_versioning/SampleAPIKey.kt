aebe24e4b264e73fad1fcedc0f81dd39d4f0a801
Best <nakarin@ekoapp.com>
2020-12-24 18:36:20 +0700

    Move API key to resource file

--- sdk-versioning/src/main/java/com/ekoapp/sdk_versioning/SampleAPIKey.kt
+++ sdk-versioning/src/main/java/com/ekoapp/sdk_versioning/SampleAPIKey.kt
@@ -1,10 +1,12 @@
 package com.ekoapp.sdk_versioning
 
+import android.content.Context
+
 
 object SampleAPIKey {
 
-    fun get(): String {
-        return "b3babb5e32dbf2314935d81e0301438fd85b84b7b2316a2e"
+    fun get(context: Context): String {
+        return context.getString(R.string.sdk_api_key)
     }
 
 }
